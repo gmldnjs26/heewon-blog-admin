3198371a50a10bc47b5b2028d074d25dde9d3f62
gmldnjs26 <imleesky265@gmail.com>
2022-12-17 21:08:19 +0900

    feat: Create Button Component

---
+++ components/atoms/Button.vue
@@ -0,0 +1,109 @@
+<template>
+  <button
+    class="border px-3 py-[6px] rounded-[3px] transition duration-250 ease-in-out flex items-center justify-center"
+    :style="`width: ${width}`"
+    :class="styles"
+    :disabled="disabled"
+    @click="onClick"
+  >
+    <Icon class="mr-1" v-if="icon" :name="icon" size="24" />
+    {{ text }}
+  </button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  text: string
+  type?: 'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'danger'
+  size?: 'small' | 'medium' | 'large'
+  width?: string
+  icon?: string
+  disabled?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  text: '',
+  type: 'primary',
+  size: 'medium',
+  width: '',
+  icon: '',
+  disabled: false,
+})
+
+const emit = defineEmits(['click'])
+
+const onClick = () => {
+  emit('click')
+}
+
+// https://tailwindcss.com/docs/content-configuration#dynamic-class-names
+// 동적 클래스 이름을 쓸 때는 중간에 javascript 코드를 끼워 넣을수는 없다.
+const styles = computed<string>(() => {
+  const classes = []
+  // button color styles
+  console.log(props.type)
+  switch (props.type) {
+    case 'primary': {
+      classes.push(
+        'text-primary-color bg-primary-bg border-primary-border-color',
+        'hover:text-primary-hover-color hover:bg-primary-hover-bg hover:border-primary-hover-border-color',
+        'disabled:text-primary-disabled-color disabled:bg-primary-disabled-bg disabled:border-primary-disabled-border-color'
+      )
+      break
+    }
+    case 'secondary': {
+      classes.push(
+        'text-secondary-color bg-secondary-bg border-secondary-border-color',
+        'hover:text-secondary-hover-color hover:bg-secondary-hover-bg hover:border-secondary-hover-border-color',
+        'disabled:text-secondary-disabled-color disabled:bg-secondary-disabled-bg disabled:border-secondary-disabled-border-color'
+      )
+      break
+    }
+    case 'success': {
+      classes.push(
+        'text-success-color bg-success-bg border-success-border-color',
+        'hover:text-success-hover-color hover:bg-success-hover-bg hover:border-success-hover-border-color',
+        'disabled:text-success-disabled-color disabled:bg-success-disabled-bg disabled:border-success-disabled-border-color'
+      )
+      break
+    }
+    case 'info': {
+      classes.push(
+        'text-info-color bg-info-bg border-info-border-color',
+        'hover:text-info-hover-color hover:bg-info-hover-bg hover:border-info-hover-border-color',
+        'disabled:text-info-disabled-color disabled:bg-info-disabled-bg disabled:border-info-disabled-border-color'
+      )
+      break
+    }
+    case 'warning': {
+      classes.push(
+        'text-warning-color bg-warning-bg border-warning-border-color',
+        'hover:text-warning-hover-color hover:bg-warning-hover-bg hover:border-warning-hover-border-color',
+        'disabled:text-warning-disabled-color disabled:bg-warning-disabled-bg disabled:border-warning-disabled-border-color'
+      )
+      break
+    }
+    case 'danger': {
+      classes.push(
+        'text-danger-color bg-danger-bg border-danger-border-color',
+        'hover:text-danger-hover-color hover:bg-danger-hover-bg hover:border-danger-hover-border-color',
+        'disabled:text-danger-disabled-color disabled:bg-danger-disabled-bg disabled:border-danger-disabled-border-color'
+      )
+      break
+    }
+  }
+  // button size styles
+  switch (props.size) {
+    case 'small': {
+      classes.push('text-sm')
+    }
+    case 'medium': {
+      classes.push('text-base')
+    }
+    case 'large': {
+      classes.push('text-large')
+    }
+  }
+  return classes.join(' ')
+})
+</script>
