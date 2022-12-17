76b0ee9012c2f8ee8c34b0fbda052211dc594121
gmldnjs26 <imleesky265@gmail.com>
2022-12-17 21:09:12 +0900

    style: add custom color and set layout

---
+++ components/organism/Sidebar.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav class="shadow w-[250px] bg-white">
+    <h2 class="text-lg">heewon</h2>
+  </nav>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,14 @@
 <template>
-  <OrganismHeader />
-  <NuxtPage />
-  <OrganismFooter />
+  <div class="bg-bgc flex w-screen h-screen">
+    <OrganismSidebar />
+    <div class="w-[1000px] mx-auto">
+      <OrganismHeader />
+      <NuxtPage />
+      <OrganismFooter />
+    </div>
+  </div>
 </template>
 
-<script setup></script>
-
-<style lang="scss" scoped></style>
+<script setup lang="ts">
+const best = ref('asd')
+</script>
