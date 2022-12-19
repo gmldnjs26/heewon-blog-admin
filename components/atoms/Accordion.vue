dcd490fe2b4e26341e72b7ff7a2afb6c5b818bf6
gmldnjs26 <imleesky265@gmail.com>
2022-12-19 20:50:51 +0900

    feat: Add Accordion Component

---
+++ components/atoms/Accordion.vue
@@ -0,0 +1,31 @@
+<template>
+  <Transition
+    @before-enter="beforeEnter"
+    @before-leave="beforeLeave"
+    @enter="enter"
+    @leave="leave"
+  >
+    <div v-show="isOpen">
+      <slot></slot>
+    </div>
+  </Transition>
+</template>
+
+<script setup lang="ts">
+import { useTransition } from '~~/composables/useTransition'
+
+interface Props {
+  isOpen: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  isOpen: false,
+})
+
+const {
+  accordionBeforeEnter: beforeEnter,
+  accordionBeforeLeave: beforeLeave,
+  accordionEnter: enter,
+  accordionLeave: leave,
+} = useTransition()
+</script>
