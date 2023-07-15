c5c76f59985c53a75f1b2385c9176fa64f7621b9
gmldnjs26 <imleesky265@gmail.com>
2023-07-15 11:36:44 +0900

    fix: checkbox

--- components/atoms/input/CheckBox.vue
+++ components/atoms/input/CheckBox.vue
@@ -1,23 +1,22 @@
 <script setup lang="ts">
 interface Props {
-  id: string
   text?: string
   checked?: boolean
-  halfChecked?: boolean
+  indeterminate?: boolean
   disabled?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  id: '',
   text: '',
   checked: false,
-  halfChecked: false,
+  indeterminate: false,
   disabled: false,
 })
 
 const emit = defineEmits(['change'])
 
 const styles = computed(() => {
+  // indeterminate style 구현
   if (props.checked) {
     return 'border-blue-dark bg-blue-dark before:absolute before:mb-1 before:w-[10px] before:h-[4.5px] before:border-white before:border-b-2 before:border-l-2 before:-rotate-45'
   } else {
@@ -33,7 +32,6 @@ const onChange = (e: Event & { target: HTMLInputElement }) => {
 <template>
   <label class="inline-flex items-center" :class="{ 'opacity-50': disabled }">
     <input
-      :id="id"
       class="mr-1 inline-flex h-4 w-4 appearance-none items-center justify-center rounded border border-solid transition-all"
       :class="styles"
       type="checkbox"
